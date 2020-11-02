fa6f386c70db3feb79d74cf7a478f4561949e945
EricBalingit <EricBalingit@users.noreply.github.com>
2020-11-02 13:08:05 -0500

    fixed ignore trailing space, don't throw on empty command sequence

--- command-parser.js
+++ command-parser.js
@@ -11,14 +11,20 @@ module.exports = function parseCommand ( input ) {
             },
             handleState = {
                 start: input => {
-                    // consume command token
-                    if ( match = input.match ( command ) ) {
-                        result.command = match [ 0 ];
-                        current_state = 'args';
-                        return input.slice ( match [ 0 ].length );
-                    }
 
-                    throw new Error ( 'Command Parser: invalid command syntax "${match[0]}"' );
+                    // ignore empty command sequence
+                    if ( input ) {
+                        // consume command token
+                        if ( match = input.match ( command ) ) {
+                            result.command = match [ 0 ];
+                            current_state = 'args';
+                            return input.slice ( match [ 0 ].length );
+                        }
+
+                        throw new Error ( 'Command Parser: invalid command syntax "${match[0]}"' );
+                    } else {
+                        return input;
+                    }
                 },
                 args: input => {
                     // ignore whitespace
@@ -26,22 +32,27 @@ module.exports = function parseCommand ( input ) {
                         input = input.slice ( match [ 0 ].length );
                     }
 
-                    if ( match = input.match ( argument ) ) {       // consume argument if one is available
-                        // unescape quotes
-                        match [ 0 ] = match [ 0 ].replace ( /\\"/g, '"' ).replace ( /\\'/g, "'" );
+                    // ignore trailing whitespace
+                    if ( input ) {
+                        if ( match = input.match ( argument ) ) {       // consume argument if one is available
+                            // unescape quotes
+                            match [ 0 ] = match [ 0 ].replace ( /\\"/g, '"' ).replace ( /\\'/g, "'" );
 
-                        if ( current_flag ) {
-                            result.flags [ current_flag ].push ( match [ 0 ] );
-                        } else {
-                            result.args.push ( match [ 0 ] );
+                            if ( current_flag ) {
+                                result.flags [ current_flag ].push ( match [ 0 ] );
+                            } else {
+                                result.args.push ( match [ 0 ] );
+                            }
+                            return input.slice ( match [ 0 ].length );
+                        } else if ( input.startsWith ( '-' ) ) {
+                            current_state = 'flags';
+                            return input;
                         }
-                        return input.slice ( match [ 0 ].length );
-                    } else if ( input.startsWith ( '-' ) ) {
-                        current_state = 'flags';
+
+                        throw new Error ( 'Command Parser: invalid argument syntax "${match[0]}"' );
+                    } else {
                         return input;
                     }
-
-                    throw new Error ( 'Command Parser: invalid argument syntax "${match[0]}"' );
                 },
                 flags: input => {
                     // ignore whitespace
@@ -49,17 +60,22 @@ module.exports = function parseCommand ( input ) {
                         input = input.slice ( match [ 0 ].length );
                     }
 
-                    if ( match = input.match ( flag ) ) {       // consume argument if one is available
-                        if ( match [ 1 ] in result.flags ) {
-                            throw new Error ( 'Command Parser: invalid repeated flag "${match[0]}"' );
-                        } else {
-                            current_flag = match [ 1 ];
-                            result.flags [ current_flag ] = [];
-                        }
+                    // ignore trailing whitespace
+                    if ( input ) {
+                        if ( match = input.match ( flag ) ) {       // consume argument if one is available
+                            if ( match [ 1 ] in result.flags ) {
+                                throw new Error ( 'Command Parser: invalid repeated flag "${match[0]}"' );
+                            } else {
+                                current_flag = match [ 1 ];
+                                result.flags [ current_flag ] = [];
+                            }
 
-                        return input.slice ( match [ 0 ].length );
+                            return input.slice ( match [ 0 ].length );
+                        } else {
+                            current_state = 'args';
+                            return input;
+                        }
                     } else {
-                        current_state = 'args';
                         return input;
                     }
                 }
